d38a44cb030a5846e1b874697f690f37230cab4a
RamssCR <ramsesgpm@gmail.com>
2025-08-10 15:10:05 -0500

    fix: corrected rules

--- src/helpers/rules/bestPractices.ts
+++ src/helpers/rules/bestPractices.ts
@@ -1,8 +1,6 @@
 import type { Rules } from '@@types/rules'
 
 export const rules: Rules[] = [
-  { label: 'no-console', check: (env) => Boolean(env.window.console) },
-  { label: 'no-alert', check: (env) => Boolean(env.window.alert) },
   { label: 'device-memory', check: (env) => 'deviceMemory' in env.navigator },
   { label: 'hardware-concurrency', check: (env) => 'hardwareConcurrency' in env.navigator },
   { label: 'secure-context', check: (env) => env.window.isSecureContext },
